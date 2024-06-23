dd3f6d851089adef9b2e04145c34bc898d48639c
Dhananjay Kumar <139283219+dhananjaykmr1991@users.noreply.github.com>
2024-06-24 02:53:50 +0530

    code update

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 
+
+load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
@@ -16,4 +18,13 @@ prompt =ChatPromptTemplate.from_messages(
     ]
 )
 
+st.title('Langchain Demo with OPENAI')
+input_text = st.text_input("Search the topic you want")
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
 
